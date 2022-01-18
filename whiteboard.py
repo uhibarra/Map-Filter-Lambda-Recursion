a384a2ac26f19a7b8d88acf5062fa69e806559e8
uhibarra <uhibarra@gmail.com>
2022-01-18 14:59:03 -0700

    first commit

---
+++ whiteboard.py
@@ -0,0 +1,19 @@
+# Fizz Buzz #2
+# Write a function to print all numbers 1 to n, but there are some constraints
+# If the number is divisible by 3, print 'Fizz' instead of the number
+# If the number is divisible 5, print 'Buzz' instead of the number
+# If the number is divisible by both 3 and 5, print 'FizzBuzz' instead of the number
+# Otherwise, simply print the number
+
+def fizzBuzz(n):
+    for num in range(1,n+1):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzBuzz(15)
